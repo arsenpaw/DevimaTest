ec161773023f80091c09956cea3c90ad9acac601
Arsen Pavliuk <arsenpaw7@gmail.com>
2024-09-08 12:20:07 +0300

    add swager to prod

--- StarWarsWebApi/StarWarsWebApi/Program.cs
+++ StarWarsWebApi/StarWarsWebApi/Program.cs
@@ -42,11 +42,10 @@ builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 
 app.UseAuthorization();
